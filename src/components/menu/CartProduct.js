45f92d5646913843f22a0908860357dd63d2e138
unkn0w_ind <100840597+unkn0wIND@users.noreply.github.com>
2023-12-21 23:10:07 +0100

    Update CartProduct.js

--- src/components/menu/CartProduct.js
+++ src/components/menu/CartProduct.js
@@ -20,7 +20,7 @@ export default function CartProduct({ product, index, onRemove }) {
                                 {product.extras?.length > 0 && (
                                     <div className="text-sm text-gray-500">
                                         {product.extras.map(extra => (
-                                            <div>{extra.name} {extra.price}€</div>
+                                            <div key={extra.name}>{extra.name} {extra.price}€</div>
                                         ))}
                                     </div>
                                 )}
